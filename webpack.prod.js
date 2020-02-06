f5bebd21c9d74502a510dac571a3a6a0a1bb082c
Gall-oDrone <piehavok@hotmail.com>
2020-02-06 11:04:39 -0600

    h3

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,14 +3,16 @@ const common = require("./webpack.common");
 const merge = require("webpack-merge");
 
 module.exports = {
-   mode: "development",
+   mode: "production",
    devtool: "none",
    entry: {
       // vendor: ['react', 'react-dom', 'antd'],
       app: './src/index.js',
    },
    output: {
-      filename: 'index_bundle.[contentHash].js',
+      filename: 'index_bundle.[contentHash:7].js',
       path: path.resolve(__dirname, 'bundle'),
+      // path.resolve(__dirname, 'dist/static'),
+      // publicPath: 'static/',
       publicPath: '/'
    }
