dc5ee9e9e9ead1eca54d192b2c9e4a996d5118dd
Gall-oDrone <piehavok@hotmail.com>
2020-02-06 22:44:24 -0600

    h29

--- home/settings/base.py
+++ home/settings/base.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
 print("BASE_DIR")
-print([os.path.join(BASE_DIR, 'dist111111')])
+print([os.path.join(BASE_DIR, 'dist')])
 
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 DEBUG = True
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'home.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'distEHKJHK')],
+        'DIRS': [os.path.join(BASE_DIR, 'dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
