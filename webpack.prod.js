bfec384d3e3031341ea6b6b4e8ebc26ff652643c
Gall-oDrone <piehavok@hotmail.com>
2020-02-07 14:53:30 -0600

    h1

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 var HtmlWebpackPlugin = require("html-webpack-plugin");
