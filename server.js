376ea834baa7d452969fe97117eb34c6ab2a505b
Gall-oDrone <piehavok@hotmail.com>
2020-02-08 17:30:32 -0600

    e2

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.get('*', (req, res) => {
 });
 
 //###
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'dist')));// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, 'dist')));// Handle React routing, return all requests to React app
+//   app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+//   });
+// }
 //###
 
 app.listen(port);
