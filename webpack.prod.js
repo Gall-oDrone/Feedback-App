1821f11ec6e127473438d47ddeff19b7fdd2c400
Gall-oDrone <piehavok@hotmail.com>
2020-02-08 14:34:31 -0600

    3e

--- webpack.prod.js
+++ webpack.prod.js
@@ -42,7 +42,7 @@ module.exports = merge(common, {
           use: [
             MiniCssExtractPlugin.loader, //3. Extract css into files
             "css-loader", //2. Turns css into commonjs
-            "sass-loader" //1. Turns sass into css
+            "sass-loader" //1. Turns sass into css 11
           ]
         }
       ]
