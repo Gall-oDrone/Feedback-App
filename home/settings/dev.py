29a3e9a4f2c415e8d23f20d6f16e6dc34e0f0e92
Gall-oDrone <piehavok@hotmail.com>
2020-03-15 19:16:07 -0600

    h

--- home/settings/dev.py
+++ home/settings/dev.py
@@ -25,16 +25,16 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': "d2f0i5ch7mim3a",
-#         'USER': "fprocgmtgzampz",
-#         'PASSWORD': "1309fcda6332a3961b01fb8a47ba69f0138d0fecb60b43b1b46edac18e175f5a",
-#         'HOST': 'ec2-54-210-128-153.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
+H_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "d2f0i5ch7mim3a",
+        'USER': "fprocgmtgzampz",
+        'PASSWORD': "1309fcda6332a3961b01fb8a47ba69f0138d0fecb60b43b1b46edac18e175f5a",
+        'HOST': 'ec2-54-210-128-153.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
