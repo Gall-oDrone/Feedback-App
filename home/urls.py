c36c0858b86714ff65dc9de8edee416dac1582dc
Gall-oDrone <piehavok@hotmail.com>
2020-03-17 20:36:40 -0600

    h

--- home/urls.py
+++ home/urls.py
@@ -27,11 +27,11 @@ urlpatterns = [
     
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL,
+#                           document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
         
 if not settings.DEBUG:
     urlpatterns += [re_path(r'^.*',
